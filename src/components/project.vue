2ba743b6886ac1fee95cf1191889e0ff3bb07c57
jungleft <77954306+jungleft@users.noreply.github.com>
2021-02-04 10:38:26 +0800

    i

---
+++ src/components/project.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="hello" :class="{dark:dark}">
+    <div id="d">
+      <input type="checkbox" v-model="dark" />黑暗模式
+    </div>
+    <h2>my works</h2>
+    <div v-for="i in items" :key="i">
+      <img :src="i">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    next() {
+      this.idx += 1
+      if (this.idx === this.items.length) {
+        this.idx = 0
+      }
+    },
+    move() {
+      this.top += 1 * this.dir2
+      this.left += 1 * this.dir
+      if(this.left == window.innerWidth - 300 || this.left == 0) {
+        this.dir *= -1
+        this.next()
+      }
+      if(this.top == window.innerHeight - 200 || this.top == 0) {
+        this.dir2 *= -1
+        this.next()
+      }
+    }
+  },
+  mounted() {
+    setInterval(this.move, 15)
+  },
+  data() {
+    return {
+      dark: false,
+      left: 0,
+      top: 0,
+      dir: 1,
+      dir2: 1,
+      idx: 0,
+      items: ['c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_0.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_1.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_2.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_3.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_4.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_5.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_6.jpg', 'c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_7.jpg', ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.hello {
+  padding-top: 1.5em;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  left: 0;
+}
+
+.dark {
+  background-color: black;
+}
+#d {
+  position: fixed;
+  bottom: 3em;
+  left: 1em;
+}
+
+h2 {
+ 
+}
+p {
+
+}
+img {
+  width: 300px;
+  position: fixed;
+  z-index: -9;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
