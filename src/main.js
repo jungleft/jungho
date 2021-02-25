89c390d443946c8d7183d2667ab98db6a8975c0b
Bestian Tang <bestian@gmail.com>
2021-02-25 10:06:04 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { firestorePlugin } from 'vuefire'
+
+Vue.use(firestorePlugin)
 
 Vue.config.productionTip = false
 
