1c23e84b9f877781b26371525dc6eb433652a893
Bestian Tang <bestian@gmail.com>
2021-03-10 20:32:03 +0800

    deploy

---
+++ precache-manifest.1edb55336d4ba0469fb417158921c00f.js
@@ -0,0 +1,638 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f117acadf473cfc108",
+    "url": "/jungho/css/about.e4dd368a.css"
+  },
+  {
+    "revision": "16f117acadf473cfc108",
+    "url": "/jungho/css/about.e4dd368a.css.map"
+  },
+  {
+    "revision": "9fbf27a65060ef27663b",
+    "url": "/jungho/css/app.2fd39a16.css"
+  },
+  {
+    "revision": "9fbf27a65060ef27663b",
+    "url": "/jungho/css/app.2fd39a16.css.map"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/jungho/favicon.ico"
+  },
+  {
+    "revision": "758c46fa6a73a173a27049f4563fe63e",
+    "url": "/jungho/img/IMG_9036.jpeg"
+  },
+  {
+    "revision": "48cb18d882998a9d22cfd20628323ac7",
+    "url": "/jungho/img/IMG_9037.jpeg"
+  },
+  {
+    "revision": "ed10753d76f088105e30d8c7e1ba195c",
+    "url": "/jungho/img/IMG_9038.jpeg"
+  },
+  {
+    "revision": "56629ca7eeaeb9b0afda8681dc265681",
+    "url": "/jungho/img/IMG_9142.jpeg"
+  },
+  {
+    "revision": "664ab0335098efdb5d8e2d200fbb4dc6",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_0.jpg"
+  },
+  {
+    "revision": "577bf7f74db0dc4f30c7ed0ea879cd31",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_1.jpg"
+  },
+  {
+    "revision": "dfcd2ceb61017848b2dbc483538d8325",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_2.jpg"
+  },
+  {
+    "revision": "1e8e7097dd5600eae3b42d8648948b93",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_3.jpg"
+  },
+  {
+    "revision": "4234788d504675c3e35e1ed8e6faa30d",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_4.jpg"
+  },
+  {
+    "revision": "407acb08b514df8e5b54483608e05b57",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_5.jpg"
+  },
+  {
+    "revision": "4505d4259f134e74603368700b875642",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_6.jpg"
+  },
+  {
+    "revision": "13a58f5cdd7af137fb8850b829c7ac61",
+    "url": "/jungho/img/c48c4fa8b906854d6327b27f30b1d24ca_4620693218563810026_210127_7.jpg"
+  },
+  {
+    "revision": "ed4a94c8d816c35aca530c4bd55b4d2f",
+    "url": "/jungho/img/feb0/img824.jpg"
+  },
+  {
+    "revision": "d8a5f379e3b93ff4de25c320b9b58a05",
+    "url": "/jungho/img/feb0/img825.jpg"
+  },
+  {
+    "revision": "798b000babd0a5766d87262cc5be0e6b",
+    "url": "/jungho/img/feb0/img826.jpg"
+  },
+  {
+    "revision": "2a5c638c6e5e1cd05ece8ac050a5dcf3",
+    "url": "/jungho/img/feb0/img827.jpg"
+  },
+  {
+    "revision": "252e8e84110d2949c731199e4aa0e524",
+    "url": "/jungho/img/feb0/img828.jpg"
+  },
+  {
+    "revision": "aff86fc6e47d9fd534a51d03b5a58879",
+    "url": "/jungho/img/feb0/img829.jpg"
+  },
+  {
+    "revision": "2afc40a640a9f633aafff308cbe47e56",
+    "url": "/jungho/img/feb0/img830.jpg"
+  },
+  {
+    "revision": "f02b5b6e013c708400fb2718ae10cb47",
+    "url": "/jungho/img/feb0/img831.jpg"
+  },
+  {
+    "revision": "3e23ab0290eb9c87c837be578adc8415",
+    "url": "/jungho/img/feb0/img832.jpg"
+  },
+  {
+    "revision": "98ab5d354ec52889214052c3a1377a65",
+    "url": "/jungho/img/feb0/img833.jpg"
+  },
+  {
+    "revision": "353d95c9a13e3986553302d00cb43467",
+    "url": "/jungho/img/feb0/img834.jpg"
+  },
+  {
+    "revision": "f9d811c0fb0aa11a93084fc7fb1dcef3",
+    "url": "/jungho/img/feb0/img835.jpg"
+  },
+  {
+    "revision": "4a6067d11f84b7fb93827e4bee301919",
+    "url": "/jungho/img/feb0/img836.jpg"
+  },
+  {
+    "revision": "d0d4c887724f6fd29245dd00219b5d84",
+    "url": "/jungho/img/feb0/img837.jpg"
+  },
+  {
+    "revision": "9c8cd6b786b5c5ffb190df98dd6c3010",
+    "url": "/jungho/img/feb0/img838.jpg"
+  },
+  {
+    "revision": "b953ed56cc23cf096e9ac1c018baffea",
+    "url": "/jungho/img/feb0/img839.jpg"
+  },
+  {
+    "revision": "69568ab572a56d2a2db24368cf3fbb33",
+    "url": "/jungho/img/feb0/img840.jpg"
+  },
+  {
+    "revision": "9437253d70ae34a116efaf07deba05dc",
+    "url": "/jungho/img/feb0/img841.jpg"
+  },
+  {
+    "revision": "d8f2e8dae4c3500a72af4fe2276eb2be",
+    "url": "/jungho/img/feb0/img842.jpg"
+  },
+  {
+    "revision": "8e70d8666bb72fb926bc163f197b87b2",
+    "url": "/jungho/img/feb0/img843.jpg"
+  },
+  {
+    "revision": "4e3b2969ab8b35acf6e1ee3920c515ed",
+    "url": "/jungho/img/feb0/img844.jpg"
+  },
+  {
+    "revision": "c36e3ccce620ea6406630802d9d38fb8",
+    "url": "/jungho/img/feb0/img845.jpg"
+  },
+  {
+    "revision": "4416d333ec57f33c1c479467bb7eef2b",
+    "url": "/jungho/img/feb0/img846.jpg"
+  },
+  {
+    "revision": "fa23e04fc6d01049a0bfa51d0deb6765",
+    "url": "/jungho/img/feb0/img847.jpg"
+  },
+  {
+    "revision": "29775e5b0d7c939f094b2af890828853",
+    "url": "/jungho/img/feb0/img848.jpg"
+  },
+  {
+    "revision": "7817fb5ae8f83c9ae5621aa72863072d",
+    "url": "/jungho/img/feb0/img849.jpg"
+  },
+  {
+    "revision": "1d4fb00540c2e595cd7376e30cff0c41",
+    "url": "/jungho/img/feb0/img850.jpg"
+  },
+  {
+    "revision": "bd8c9e7ec9658e50d7762e3c47a1f570",
+    "url": "/jungho/img/feb0/img851.jpg"
+  },
+  {
+    "revision": "8b222c5cd32af771e847344dd12e3ac4",
+    "url": "/jungho/img/feb1/img824.jpg"
+  },
+  {
+    "revision": "5d00a1e96f0313a13cce3079a4e867da",
+    "url": "/jungho/img/feb1/img825.jpg"
+  },
+  {
+    "revision": "3a008a12e6f54adc9c1781f7ef5d0dcb",
+    "url": "/jungho/img/feb1/img826.jpg"
+  },
+  {
+    "revision": "4e5263f85a0e689cca4ec3cdad35caa7",
+    "url": "/jungho/img/feb1/img827.jpg"
+  },
+  {
+    "revision": "f9d89db0377127c1fb55640acdad1238",
+    "url": "/jungho/img/feb1/img828.jpg"
+  },
+  {
+    "revision": "7359b14f963845b11475ac7b6fa9f153",
+    "url": "/jungho/img/feb1/img829.jpg"
+  },
+  {
+    "revision": "a52029995217238fd8f4692876b5f51c",
+    "url": "/jungho/img/feb1/img830.jpg"
+  },
+  {
+    "revision": "5bf462b6bda3aadbf922d63fe92ba2b3",
+    "url": "/jungho/img/feb1/img831.jpg"
+  },
+  {
+    "revision": "f9ae2c7b995a63ef2373e25ff59eb1de",
+    "url": "/jungho/img/feb1/img832.jpg"
+  },
+  {
+    "revision": "7f490748bd2f7af7e43cfc31d0999e30",
+    "url": "/jungho/img/feb1/img833.jpg"
+  },
+  {
+    "revision": "bb25c219b7a1c108800ba236d3da9908",
+    "url": "/jungho/img/feb1/img834.jpg"
+  },
+  {
+    "revision": "4c41d976316b2a6d9c7f684ee0717a03",
+    "url": "/jungho/img/feb1/img835.jpg"
+  },
+  {
+    "revision": "3beb35c0ae6e5690250a52a2cf0a4891",
+    "url": "/jungho/img/feb1/img836.jpg"
+  },
+  {
+    "revision": "b4fc2376a38b6fe1cadd76bf13c3533d",
+    "url": "/jungho/img/feb1/img837.jpg"
+  },
+  {
+    "revision": "802c6733f278b91c5fd1872f66f78e1c",
+    "url": "/jungho/img/feb1/img838.jpg"
+  },
+  {
+    "revision": "86ff277ea9248d5e7fbe65bc3ff00df2",
+    "url": "/jungho/img/feb1/img839.jpg"
+  },
+  {
+    "revision": "81fc2e8be99b0a71a2d3f571f97e7c83",
+    "url": "/jungho/img/feb1/img840.jpg"
+  },
+  {
+    "revision": "5ffadbc4e342d9eb326d8099351498b3",
+    "url": "/jungho/img/feb1/img841.jpg"
+  },
+  {
+    "revision": "893dbe7d3fe7b8e835e35c0a0f1109e4",
+    "url": "/jungho/img/feb1/img842.jpg"
+  },
+  {
+    "revision": "d182db9319ea9b2cfed43db81f112117",
+    "url": "/jungho/img/feb1/img843.jpg"
+  },
+  {
+    "revision": "0fe62a8ef8fea3b2519b10028e9d1058",
+    "url": "/jungho/img/feb1/img844.jpg"
+  },
+  {
+    "revision": "1d871cd6d76d81d62135e82514afb97d",
+    "url": "/jungho/img/feb1/img845.jpg"
+  },
+  {
+    "revision": "a495043f535ec2144620b936b3aa8145",
+    "url": "/jungho/img/feb1/img846.jpg"
+  },
+  {
+    "revision": "6af5ed0ffcb0b3665cc192a6359b7a20",
+    "url": "/jungho/img/feb1/img847.jpg"
+  },
+  {
+    "revision": "9359d6996332c49c0e2fd8a921984445",
+    "url": "/jungho/img/feb1/img848.jpg"
+  },
+  {
+    "revision": "427d6f71e44562e059528ae13cf7ca65",
+    "url": "/jungho/img/feb1/img849.jpg"
+  },
+  {
+    "revision": "f97f22fbce1a346a0eeaaaefd3432753",
+    "url": "/jungho/img/feb1/img850.jpg"
+  },
+  {
+    "revision": "4b6b41b8f36c95c84af1191427244fce",
+    "url": "/jungho/img/feb1/img851.jpg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/jungho/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/jungho/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/jungho/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/jungho/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/jungho/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/jungho/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/jungho/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/jungho/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/jungho/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/jungho/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/jungho/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/jungho/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/jungho/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/jungho/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/jungho/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "7d1f02488da1f756cfa35e240d367de2",
+    "url": "/jungho/img/jan0/01.jpg"
+  },
+  {
+    "revision": "18b65af0acb8f57348853f63fa7db881",
+    "url": "/jungho/img/jan0/02.jpg"
+  },
+  {
+    "revision": "79432ee338f3d9b84c0a168ccab81f3a",
+    "url": "/jungho/img/jan0/03.jpg"
+  },
+  {
+    "revision": "38bdfee0763e128f79d3fe2c43200728",
+    "url": "/jungho/img/jan0/04.jpg"
+  },
+  {
+    "revision": "3e02736dd0ee1ee0ea6c72abcbc6c576",
+    "url": "/jungho/img/jan0/05.jpg"
+  },
+  {
+    "revision": "31d13ccebf3a69f327d94d90cfb45720",
+    "url": "/jungho/img/jan0/06.jpg"
+  },
+  {
+    "revision": "1cfcb78e2b48eba239cd64d8e48ebb63",
+    "url": "/jungho/img/jan0/07.jpg"
+  },
+  {
+    "revision": "3dd706d60e38080409a8250b64153649",
+    "url": "/jungho/img/jan0/08.jpg"
+  },
+  {
+    "revision": "a896a98022f1e5bf49a46c3b295c504f",
+    "url": "/jungho/img/jan0/09.jpg"
+  },
+  {
+    "revision": "e54aa7b8c420acfd6c8d3e6ea3f235e0",
+    "url": "/jungho/img/jan0/10.jpg"
+  },
+  {
+    "revision": "19d59dc65702868dee98cdd48f55d63e",
+    "url": "/jungho/img/jan0/img499.jpg"
+  },
+  {
+    "revision": "44926c3720b97edcc75c755fdc9c39c5",
+    "url": "/jungho/img/jan0/img500.jpg"
+  },
+  {
+    "revision": "3e385644ab0084e1e87e9d695cf8936d",
+    "url": "/jungho/img/jan0/img501.jpg"
+  },
+  {
+    "revision": "71c18e8ab66059f77f62f84633e37403",
+    "url": "/jungho/img/jan0/img502.jpg"
+  },
+  {
+    "revision": "72d88d908fa87a0eec6b0504a6bf92bf",
+    "url": "/jungho/img/jan0/img503.jpg"
+  },
+  {
+    "revision": "71c4c5c0313abbd1bb0502d134d8980d",
+    "url": "/jungho/img/jan0/img504.jpg"
+  },
+  {
+    "revision": "dc0a11ee42d33edc4eecc108a580906b",
+    "url": "/jungho/img/jan0/img505.jpg"
+  },
+  {
+    "revision": "5b0da01dbe29c5a4762ff824e5b6a312",
+    "url": "/jungho/img/jan0/img506.jpg"
+  },
+  {
+    "revision": "1965cb16fa48b7f3c1d8e0c41cb21d04",
+    "url": "/jungho/img/jan0/img507.jpg"
+  },
+  {
+    "revision": "7a7f3545cc9d9a53200a49a2aa11381d",
+    "url": "/jungho/img/jan0/img508.jpg"
+  },
+  {
+    "revision": "0a49a2edf49834b6b757b00a990a065f",
+    "url": "/jungho/img/jan0/img509.jpg"
+  },
+  {
+    "revision": "4a257ce411c98a56e4e2f6d1cc2d3592",
+    "url": "/jungho/img/jan0/img510.jpg"
+  },
+  {
+    "revision": "db0c864d335619779cf1e7fd11df1f7d",
+    "url": "/jungho/img/jan0/img511.jpg"
+  },
+  {
+    "revision": "d59ef6a1a9966d3448dd9b0504a8b97b",
+    "url": "/jungho/img/jan0/img512.jpg"
+  },
+  {
+    "revision": "be8deb8447aace056092125bf20ddb19",
+    "url": "/jungho/img/jan0/img513.jpg"
+  },
+  {
+    "revision": "e71da2cbb61cc8621cefecf1a75076c5",
+    "url": "/jungho/img/jan0/img514.jpg"
+  },
+  {
+    "revision": "fcf657e67f247c551e3f920026fb1f5c",
+    "url": "/jungho/img/jan0/img515.jpg"
+  },
+  {
+    "revision": "daddca5956981608a16aa7af42f5ad65",
+    "url": "/jungho/img/jan0/img516.jpg"
+  },
+  {
+    "revision": "ec80096eaa5a9b925b1caa9075182dbd",
+    "url": "/jungho/img/jan0/img517.jpg"
+  },
+  {
+    "revision": "3110e1f80ff41f69c3ff236292245cb8",
+    "url": "/jungho/img/jan0/img518.jpg"
+  },
+  {
+    "revision": "a0497fc970775a1f5bd7a7b34d8c77fc",
+    "url": "/jungho/img/jan0/img519.jpg"
+  },
+  {
+    "revision": "51ab5a968344dc35ede3948ee43ef3a6",
+    "url": "/jungho/img/jan1/01.jpg"
+  },
+  {
+    "revision": "7d1b93434bf597f2ee0ff3f408a09025",
+    "url": "/jungho/img/jan1/02.jpg"
+  },
+  {
+    "revision": "5d3abe9d9ea98b69e697d1362fdd2e5d",
+    "url": "/jungho/img/jan1/03.jpg"
+  },
+  {
+    "revision": "014f7be0daa1060fdaa884f2b93a961f",
+    "url": "/jungho/img/jan1/04.jpg"
+  },
+  {
+    "revision": "dff923015841b6b427b8dcf7ffe6b3ee",
+    "url": "/jungho/img/jan1/05.jpg"
+  },
+  {
+    "revision": "13713f4fd46f67b8e3de7d45326d4a3d",
+    "url": "/jungho/img/jan1/06.jpg"
+  },
+  {
+    "revision": "549a45c75a36d884b1c62fb8c4c371e2",
+    "url": "/jungho/img/jan1/07.jpg"
+  },
+  {
+    "revision": "36db244d7e0ce30836f705a509d5b5be",
+    "url": "/jungho/img/jan1/08.jpg"
+  },
+  {
+    "revision": "5983486abdfa4bc7888c9b24ad300e72",
+    "url": "/jungho/img/jan1/09.jpg"
+  },
+  {
+    "revision": "4adb447efd5fe0e8178be6b9f1c9351c",
+    "url": "/jungho/img/jan1/10.jpg"
+  },
+  {
+    "revision": "2977ad9249ef9822e987a7b2c0c8a480",
+    "url": "/jungho/img/jan1/img499.jpg"
+  },
+  {
+    "revision": "0b8ad15aad9602d0381dd3be7f9199ea",
+    "url": "/jungho/img/jan1/img500.jpg"
+  },
+  {
+    "revision": "67832385f1ffdecf6d901242ccb66242",
+    "url": "/jungho/img/jan1/img501.jpg"
+  },
+  {
+    "revision": "af07309f8ed2b186acad92fb8b6d8d23",
+    "url": "/jungho/img/jan1/img502.jpg"
+  },
+  {
+    "revision": "93a7e5af057a7f49f682ca1efc7a43c2",
+    "url": "/jungho/img/jan1/img503.jpg"
+  },
+  {
+    "revision": "434e825c480ff0ea05e4bfecf0fc286d",
+    "url": "/jungho/img/jan1/img504.jpg"
+  },
+  {
+    "revision": "7fb6494e3737497caaa33d640d7c1670",
+    "url": "/jungho/img/jan1/img505.jpg"
+  },
+  {
+    "revision": "06dc1d2e2f03129092a0a296275a06c3",
+    "url": "/jungho/img/jan1/img506.jpg"
+  },
+  {
+    "revision": "32a7c326b7e92da5673adb106b91bd39",
+    "url": "/jungho/img/jan1/img507.jpg"
+  },
+  {
+    "revision": "cc6b424d5b24fa99f0f6ce861a621dd1",
+    "url": "/jungho/img/jan1/img508.jpg"
+  },
+  {
+    "revision": "08e451827bdfe5b8ba8e27ba9b479f72",
+    "url": "/jungho/img/jan1/img509.jpg"
+  },
+  {
+    "revision": "6a1fae35ce3b3567f267da8c291178b6",
+    "url": "/jungho/img/jan1/img510.jpg"
+  },
+  {
+    "revision": "8851ce0779ce560f459ae575f00834b2",
+    "url": "/jungho/img/jan1/img511.jpg"
+  },
+  {
+    "revision": "0b0fb8e1880c9141bee9cbf15de17fdf",
+    "url": "/jungho/img/jan1/img512.jpg"
+  },
+  {
+    "revision": "3fa07fcf8e819feeb79230dafd66864c",
+    "url": "/jungho/img/jan1/img513.jpg"
+  },
+  {
+    "revision": "021c14d242f5b226af97a06680b6a284",
+    "url": "/jungho/img/jan1/img514.jpg"
+  },
+  {
+    "revision": "daac283d6565281c8cf4d3e2a2bdd1fc",
+    "url": "/jungho/img/jan1/img515.jpg"
+  },
+  {
+    "revision": "c4d868c2e680d42c21862e1ff37ed8ed",
+    "url": "/jungho/img/jan1/img516.jpg"
+  },
+  {
+    "revision": "526069c2cedd05956e2ab2cc5c20f344",
+    "url": "/jungho/img/jan1/img517.jpg"
+  },
+  {
+    "revision": "3ed66c7f996a5982b7ae64802227ed19",
+    "url": "/jungho/img/jan1/img518.jpg"
+  },
+  {
+    "revision": "83689d1fb9caca6ec2c35b0f7a5ec150",
+    "url": "/jungho/img/jan1/img519.jpg"
+  },
+  {
+    "revision": "2b101b7e0caa4f38f7b3c6592f8600bd",
+    "url": "/jungho/index.html"
+  },
+  {
+    "revision": "16f117acadf473cfc108",
+    "url": "/jungho/js/about.ad2c52bc.js"
+  },
+  {
+    "revision": "16f117acadf473cfc108",
+    "url": "/jungho/js/about.ad2c52bc.js.map"
+  },
+  {
+    "revision": "9fbf27a65060ef27663b",
+    "url": "/jungho/js/app.9a954f4f.js"
+  },
+  {
+    "revision": "9fbf27a65060ef27663b",
+    "url": "/jungho/js/app.9a954f4f.js.map"
+  },
+  {
+    "revision": "d9fe0ba724a534f65b15",
+    "url": "/jungho/js/chunk-vendors.a2586eb1.js"
+  },
+  {
+    "revision": "d9fe0ba724a534f65b15",
+    "url": "/jungho/js/chunk-vendors.a2586eb1.js.map"
+  },
+  {
+    "revision": "de98ee23d15a908664eb5e275d6351e8",
+    "url": "/jungho/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/jungho/robots.txt"
+  }
+]);
