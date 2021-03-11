3b4b4228e979e3f373e59a17f003a70ac5a5846e
jungleft <77954306+jungleft@users.noreply.github.com>
2021-03-11 11:43:47 +0800

    i

--- src/router/index.js
+++ src/router/index.js
@@ -59,6 +59,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../components/contact.vue')
+  },
+  {
+    path: '/scano',
+    name: 'scano',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../components/scano.vue')
   }
 ]
 
