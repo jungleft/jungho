df9c4b0b585fbee5d766240a8598591737165e36
Bestian Tang <bestian@gmail.com>
2024-12-13 13:47:37 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,17 @@ module.exports = {
   productionSourceMap: true,
   pwa: {
     workboxOptions: {
-      exclude: [/\.gitkeep/]
+      exclude: [/\.gitkeep/],
+      skipWaiting: true,
+      clientsClaim: true
+    },
+    name: 'Jungho',
+    themeColor: '#000',
+    msTileColor: '#000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      background_color: '#000'
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
